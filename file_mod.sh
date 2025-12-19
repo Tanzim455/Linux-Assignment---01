70ad37de453044dbafefcd39250f7abcbc5d7c8f
Tanzim455 <tanzim.ibthesam@gmail.com>
2025-12-19 09:53:19 +0600

    File mod successfully done

--- file_mod.sh
+++ file_mod.sh
@@ -3,13 +3,18 @@
 # file_mod.sh
 echo "=== Create a file named example.txt in the current user's home directory ==="
 cd $HOME
-touch example.txt
+sudo touch example.txt
 echo "===Create a new user without a home and shell named student.==="
-sudo useradd -M -s /sbin/nologin student
+if ! id -u student &>/dev/null; then
+    sudo useradd -M -s /usr/sbin/nologin student
+fi
 echo "===Change the owner of example.txt to a user named student.==="
-sudo chown student example.txt
+sudo chown student:student example.txt
 echo "===Change the group of example.txt to a group named student.===="
-sudo groupadd student
+if ! getent group student &>/dev/null; then
+    sudo groupadd student
+fi
+
 sudo chgrp student example.txt
 echo "Verify and display the permission changes";
 ls -l example.txt
