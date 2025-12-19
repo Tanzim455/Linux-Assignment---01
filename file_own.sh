54b12fa5c59557d8d0c4356b4375bd0ed6c458bf
Tanzim455 <tanzim.ibthesam@gmail.com>
2025-12-19 10:01:06 +0600

    File own corrected

--- file_own.sh
+++ file_own.sh
@@ -2,15 +2,15 @@
 # file_own.sh
 echo "=== Create a directory named project in the current user's home directory. ==="
 cd $HOME
-mkdir project
+mkdir -p project
 echo "===Create a file named report.txt inside the project directory==="
 cd $HOME/project
-touch report.txt
+ touch report.txt
 echo "===Set the permissions of report.txt to read and write for the owner, and read-only for the group and others.==="
 cd $HOME/project
-sudo chmod 644 report.txt
+chmod 644 report.txt
 echo "===Set the permissions of the project directory to read, write, and execute for the owner, and read and execute for the group and others==="
-sudo chmod 755 $HOME/project
+chmod 755 $HOME/project
 echo "===Verify and display the permission changes.==="
 
 ls -ld $HOME/project $HOME/project/report.txt
